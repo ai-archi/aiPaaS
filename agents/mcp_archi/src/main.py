fa194803ddcf489013c626c15d06eaf50a975e1e
jinhbm <jinhbm@yonyou.com>
2025-05-22 16:02:09 +0800

    feat:添加mcp server demo

--- agents/mcp_archi/src/main.py
+++ agents/mcp_archi/src/main.py
@@ -1,12 +1,19 @@
 from fastmcp import FastMCP
 import asyncio
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s] %(filename)s:%(lineno)d %(message)s'
+)
 
 mcp = FastMCP("FastMCP Demo Server")
 
+
 @mcp.tool()
-def add(a: int, b: int) -> int:
-    """Add two numbers"""
-    return a + b
+def echo(text: str) -> str:
+    """回显输入文本"""
+    return text
 
 if __name__ == "__main__":
     asyncio.run(mcp.run_sse_async(host="127.0.0.1", port=8000))
